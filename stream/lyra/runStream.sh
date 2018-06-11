81f9d2a4ecd76bcd4361ffd2ff786bc0cafbad54
Cameron Smith <smithc11@rpi.edu>
2018-06-11 15:13:27 -0400

    stream lyra execution script

--- stream/lyra/runStream.sh
+++ stream/lyra/runStream.sh
@@ -1,8 +1,45 @@
-source /ccs/home/cwsmith/software/aocc/setenv_AOCC.sh
-wget --no-verbose --no-parent --recursive --level=1 --no-directories https://www.cs.virginia.edu/stream/FTP/Code/
-clang -O3 -fopenmp -D_OPENMP stream.c -o stream_aocc_omp_O3
-clang -O3 -march=znver1 -flto -fuse-ld=lld -fopenmp -D_OPENMP stream.c -o stream_aocc_omp_opt
-cat /dev/null > opt.log; for i in {1..48}; do export OMP_NUM_THREADS=$i; ./stream_aocc_omp_opt >> opt.log; done
-cat /dev/null > O3.log; for i in {1..48}; do export OMP_NUM_THREADS=$i; ./stream_aocc_omp_O3 >> O3.log; done
-awk '/^Triad/ {print $2}' opt.log > opt.triad.log
-awk '/^Triad/ {print $2}' O3.log > O3.triad.log
+#!/bin/bash -e
+usage="Usage: $0 <download=0|1> <compiler=gcc|aocc>"
+[ $# -ne 2 ] && echo $usage && exit 1
+
+download=$1
+[[ "$download" != "0" && "$download" != "1" ]] && echo "download set to '$download'" && echo $usage && exit 1
+
+compiler=$2
+[[ "$compiler" != "gcc" && "$compiler" != "aocc" ]] && echo "compiler set to '$compiler'" && echo $usage && exit 1
+
+if [ "$download" == "1" ]; then
+  wget --no-verbose --no-parent --recursive --level=1 --no-directories https://www.cs.virginia.edu/stream/FTP/Code/
+fi
+
+if [ "$compiler" == "aocc" ]; then
+  source /ccs/home/cwsmith/software/aocc/setenv_AOCC.sh
+  clang -O3 -fopenmp -D_OPENMP stream.c -o stream_aocc_omp_O3
+  clang -O3 -march=znver1 -flto -fuse-ld=lld -fopenmp -D_OPENMP stream.c -o stream_aocc_omp_opt
+fi
+
+if [ "$compiler" == "gcc" ]; then
+  gcc -O3 -fopenmp -D_OPENMP stream.c -o stream_gcc_omp_O3
+  gcc -O3 -march=native -flto -fopenmp -D_OPENMP stream.c -o stream_gcc_omp_opt
+fi
+
+set -x
+
+export OMP_DISPLAY_ENV=1
+export OMP_PLACES=cores
+
+for build in opt O3; do
+  for procBind in close spread; do
+    export OMP_PROC_BIND=${procBind}
+    bin=./stream_${compiler}_omp_${build}
+    log=${build}_${compiler}_place${OMP_PLACES}_bind${OMP_PROC_BIND}.log
+    cat /dev/null > $log
+    for i in {1..48}; do
+      export OMP_NUM_THREADS=$i
+      $bin &>> $log
+    done
+    awk '/^Triad/ {print $2}' $log > $log.triad
+  done
+done
+
+set +x
